1e92dc1055f767a2ca45d54d8f900a5439435e8f
Fernando Daciuk <f.daciuk@gmail.com>
2016-12-16 12:35:57 -0200

    Default funcionality is ok

--- src/is.js
+++ src/is.js
@@ -1,5 +1,8 @@
 'use strict'
 
-const is = () => {}
+const is = (typeToTest, value) => {
+  const type = typeToTest.replace(/^\w/, (l) => l.toUpperCase())
+  return Object.prototype.toString.call(value) === `[object ${type}]`
+}
 
 export default is
--- src/is.test.js
+++ src/is.test.js
@@ -6,3 +6,11 @@ import is from './is'
 test('is should be a function', () => {
   expect(is).to.be.a('function')
 })
+
+test('is("object", {}) should return true', () => {
+  expect(is('object', {})).to.be.equal(true)
+})
+
+test('is("object", []) should return false', () => {
+  expect(is('object', [])).to.be.equal(false)
+})
