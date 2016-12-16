0d53ffd2fd68347336eeddf22b51abe747521247
Fernando Daciuk <f.daciuk@gmail.com>
2016-12-16 17:49:29 -0200

    Refactory: create aux functions

--- src/is.js
+++ src/is.js
@@ -3,16 +3,24 @@
 const is = (...params) => {
   const [typeToTest, value] = params
 
-  if (typeof typeToTest !== 'string') {
+  if (!isString(typeToTest)) {
     throw new TypeError('typeToTest must be a string')
   }
 
-  if (params.length === 1) {
+  if (!hasValue(params)) {
     return (value) => is(typeToTest, value)
   }
 
   const type = typeToTest.replace(/^\w/, (l) => l.toUpperCase())
   return Object.prototype.toString.call(value) === `[object ${type}]`
+}
+
+function isString (value) {
+  return typeof value === 'string'
+}
+
+function hasValue (params) {
+  return params.length === 2
 }
 
 export default is
