3873fab0b394b830a478429571ce30b7d4e2e2ed
Fernando Daciuk <f.daciuk@gmail.com>
2016-12-16 12:46:51 -0200

    Add all necessary tests

--- src/is.js
+++ src/is.js
@@ -1,6 +1,16 @@
 'use strict'
 
-const is = (typeToTest, value) => {
+const is = (...params) => {
+  const [typeToTest, value] = params
+
+  if (typeof typeToTest !== 'string') {
+    throw new TypeError('typeToTest must be a string')
+  }
+
+  if (params.length === 1) {
+    return (value) => is(typeToTest, value)
+  }
+
   const type = typeToTest.replace(/^\w/, (l) => l.toUpperCase())
   return Object.prototype.toString.call(value) === `[object ${type}]`
 }
--- src/is.test.js
+++ src/is.test.js
@@ -14,3 +14,36 @@ test('is("object", {}) should return true', () => {
 test('is("object", []) should return false', () => {
   expect(is('object', [])).to.be.equal(false)
 })
+
+test('is("object") should return a function (partial application)', () => {
+  expect(is('object')).to.be.a('function')
+})
+
+test('is("number")(10) should return true', () => {
+  const isNumber = is('number')
+  expect(isNumber(10)).to.be.equal(true)
+})
+
+test('is(10) should throw an TypeError: "typeToTest must be a string"', () => {
+  try {
+    is(10)
+  } catch (e) {
+    expect(e.message).to.be.equal('typeToTest must be a string')
+  }
+})
+
+test('is("undefined", undefined) should return true', () => {
+  expect(is('undefined', undefined)).to.be.equal(true)
+})
+
+test('is("undefined", null) should return false', () => {
+  expect(is('undefined', null)).to.be.equal(false)
+})
+
+test('is() should throw an TypeError: "typeToTest must be a string"', () => {
+  try {
+    is()
+  } catch (e) {
+    expect(e.message).to.be.equal('typeToTest must be a string')
+  }
+})
