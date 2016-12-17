28637ef54bfe992c85bdf296e40ba8430b10095e
Fernando Daciuk <f.daciuk@gmail.com>
2016-12-17 18:53:40 -0200

    Add banner

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 'use strict'
 
-const gulp = require('gulp')
+const fs = require('fs')
 const { spawn } = require('child_process')
+const gulp = require('gulp')
+const gutil = require('gulp-util')
+const pkg = require('./package.json')
+
 const filesToWatch = ['src/**/*.js', 'gulpfile.js']
 
 const exec = (command) => new Promise((resolve, reject) => {
@@ -17,16 +21,32 @@ const handleError = (error) => {
 
 const test = () => exec('yarn test')
 const build = () => exec('yarn build')
+
+const createAndApplyBanner = () => new Promise((resolve) => {
+  const bannerFile = fs.readFileSync('banner.txt')
+  const file = fs.readFileSync('dist/is.min.js')
+  const banner = gutil.template(bannerFile, {
+    file: isMin,
+    filename: 'is.min.js',
+    pkg
+  })
+  const content = `${banner}\n${file}`
+  fs.writeFileSync('dist/is.min.js', content)
+  resolve()
+})
+
 const add = () => exec('git add .')
 const commit = () => exec('git commit -S -m "Minifying"')
 gulp.task('preversion', () => {
   return Promise.resolve()
     .then(test)
     .then(build)
+    .then(createAndApplyBanner)
     .then(add)
     .then(commit)
     .catch(handleError)
 })
+
 
 const publish = () => exec('npm run pub')
 const update = () => exec('yarn git:update')
