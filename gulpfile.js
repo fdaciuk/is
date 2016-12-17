330905d6be818b33f0e1209fe38bd7836c7e1ac3
Fernando Daciuk <f.daciuk@gmail.com>
2016-12-17 15:34:10 -0200

    Add commands to build project

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,30 @@
 
 const gulp = require('gulp')
 const { spawn } = require('child_process')
+const filesToWatch = ['src/**/*.js', 'gulpfile.js']
 
-gulp.task('lint', (cb) => {
-  const lint = spawn('yarn', ['lint'], { stdio: 'inherit' })
-  lint.on('close', () => cb())
-})
+const exec = (commandLine, cb) => {
+  const command = commandLine.split(' ')
+  const program = command[0]
+  const params = command.slice(1)
+  const cmd = spawn(program, params, { stdio: 'inherit' })
+  cmd.on('close', () => cb())
+}
 
-gulp.task('default', ['lint'], () => {
-  gulp.watch(['src/**/*.js', 'gulpfile.js'], ['lint'])
-})
+const createTaskWithCommand = (command) => (cb) => exec(command, cb)
+
+gulp.task('test', createTaskWithCommand('yarn test'))
+gulp.task('build', createTaskWithCommand('yarn build'))
+gulp.task('add', createTaskWithCommand('git add .'))
+gulp.task('commit', createTaskWithCommand('git commit -S -m "Minifying"'))
+gulp.task('preversion', ['test', 'build', 'add', 'commit'])
+
+gulp.task('publish', createTaskWithCommand('npm run pub'))
+gulp.task('update', createTaskWithCommand('yarn git:update'))
+gulp.task('postversion', ['publish', 'update'])
+
+gulp.task('lint', createTaskWithCommand('yarn lint'))
+
+gulp.task('watch', () => gulp.watch(filesToWatch, ['lint']))
+
+gulp.task('default', ['lint', 'watch'])
